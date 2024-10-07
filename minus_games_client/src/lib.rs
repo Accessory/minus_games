1ca51017c22fb43d3ee9db77b5c175ad9ea50fce
Accessory <>
2024-10-07 14:06:39 +0200

    Update to 0.5

--- minus_games_client/src/lib.rs
+++ minus_games_client/src/lib.rs
@@ -3,8 +3,7 @@ use crate::actions::download::download;
 #[cfg(target_family = "unix")]
 use crate::actions::menu::select_game_to_play;
 use crate::actions::menu::{
-    select_download, select_game, select_game_to_delete, select_repair,
-    start_menu,
+    select_download, select_game, select_game_to_delete, select_repair, start_menu,
 };
 use crate::actions::other::{list, list_json};
 use crate::actions::repair::repair_game;
